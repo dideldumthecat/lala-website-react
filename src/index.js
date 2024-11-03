dd3628c4031e4dc8ad1691aa0b5cd9439f7f3749
Alexander Summa <info@alexandersumma.de>
2024-11-03 15:05:13 +0100

    Use SCSS again instead of temporary single CSS file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./styles/style.scss";
 
 import LalaWebsite from "./App";
 
