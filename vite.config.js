0c29c1d57ec0905d3163c3d9da75f1f0e3f16ec4
Alexander Summa <info@alexandersumma.de>
2024-11-05 11:56:31 +0100

    Adjust Vite server config to open the browser automatically for convenience

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 1056,
+        open: true,
+    },
+    preview: {
+        port: 1056,
+    },
     build: {
         sourcemap: true,
-        outDir: 'dist',
+        outDir: "dist",
     },
     css: {
         devSourcemap: true,
