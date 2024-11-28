916633ea52c8ce3f87d8e9b61d4c528cc1e2781d
Alexander Summa <info@alexandersumma.de>
2024-11-28 10:44:36 +0100

    Show a spinner while loading the tiles

--- src/components/LalaWebsite.jsx
+++ src/components/LalaWebsite.jsx
@@ -1,22 +1,29 @@
 import { useState, useEffect } from 'react';
 import ContentContainer from './ContentContainer';
+import Spinner from './Spinner';
 
 function LalaWebsite() {
     const [tiles, setTiles] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const fetchTiles = async () => {
         try {
             const response = await fetch(import.meta.env.VITE_API_URL);
             const data = await response.json();
             setTiles(data);
+            setLoading(false);
         } catch (error) {
-            console.error(error);
+            console.error('Error fetching tiles:', error);
         }
     }
 
     useEffect(() => {
         fetchTiles();
     }, []);
+
+    if (loading) {
+        return <Spinner />;
+    }
 
     return (
         <>
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import '../styles/spinner.scss';
+
+function Spinner() {
+    return (
+        <div className='spinner-container'>
+            <div className='spinner' />
+        </div>
+    );
+}
+
+export default Spinner;
