8e8c6524fd87edb6609abf8f71459891e81277d8
Alexander Summa <info@alexandersumma.de>
2026-01-16 17:08:02 +0100

    Adjust copyright date in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     const shortVersion = version.slice(0, 6);
 
     return (
-        <footer>© 2024-2025 The LALA - Crafted with care by Alexander Summa | View the code on <a href="https://github.com/dideldumthecat/lala-website-react" target="_blank">GitHub</a> | Version: [{shortVersion}]</footer>
+        <footer>© 2024-2026 The LALA - Crafted with care by Alexander Summa | View the code on <a href="https://github.com/dideldumthecat/lala-website-react" target="_blank">GitHub</a> | Version: [{shortVersion}]</footer>
     );
 }
 
